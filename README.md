0a4aea2f0aec91708d96863b6dfb38d9f1d3b840
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-04-17 01:55:24 -0500

    Setup and connect to database

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # bookselve
 
-mongodb://<dbuser>:<dbpassword>@ds135993.mlab.com:35993/heroku_m1tphfdf
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT ||3000
 const app =  express();
+// DB config
+const db = require('./config/key').mongoURL
+
+// Connect to mongoose
+mongoose
+.connect(db)
+.then(()=>console.log('MongoDB Connected'))
+.catch(err => console.log(err))
+
+//Root Router
 app.get('/',(req, res)=>{
      res.json({Hello:'Greeting from here'})
 })
+
 
 
 
